f90535548eaa21b4384d4d964a576d228dd8a558
lenkajobankova <lenka.jobi@gmail.com>
2023-11-06 18:32:51 +0100

    Vytvořena třída RestaurantManager
    Byla vytvořena třída RestaurantManager včetně všech metod dle zadání.

---
+++ src/com/engeto/restaurant/RestaurantManager.java
@@ -0,0 +1,95 @@
+package com.engeto.restaurant;
+
+import java.math.BigDecimal;
+import java.time.LocalTime;
+import java.time.temporal.ChronoUnit;
+import java.util.*;
+
+public class RestaurantManager{
+    //region Methods
+    public static int getUnpaidOrders(List<Order> listOfOrders){
+        int result = 0;
+        for (Order order : listOfOrders){
+            if (!order.isPaid()){
+                result++;
+            }
+        }
+        return result;
+    }
+    public static int getUnfinishedOrders(List<Order> listOfOrders){
+        int result = 0;
+            for(Order order : listOfOrders){
+                if (order.getFulfilmentTime()==Settings.closingTime()){
+                        result++;
+                }
+            }
+        return result;
+    }
+    public static List<Order> getListByOrderedTime(List<Order> listOfOrders){
+        List<Order> sortedList = new ArrayList<>(listOfOrders);
+        Collections.sort(sortedList);
+        return sortedList;
+    }
+    public static double getAverageProcessingTime(List<Order> listOfOrders){
+        double result = 0;
+        long minutes = 0;
+        double finalResult;
+        for (Order order : listOfOrders){
+            if (order.getFulfilmentTime() != Settings.closingTime()) {
+                minutes += ChronoUnit.MINUTES.between(order.getOrderedTime(),order.getFulfilmentTime());
+            }
+            result++;
+        }
+        finalResult = (double) minutes/result;
+        return finalResult;
+    }
+    public static Set<Dish> getListOfTodaysOrders(List<Order> listOfOrders){
+        Set<Dish> dishes = new HashSet<>();
+        for (Order order : listOfOrders){
+            dishes.add(order.getDish());
+        }
+        return dishes;
+    }
+    public static void getOneTableOrders(int tableNumber, List<Order> listOfOrders){
+        getFormattedTableNumber(tableNumber);
+        int orderNumber = 1;
+        for (Order order : listOfOrders){
+            if (order.getTableNumber() == tableNumber){
+                System.out.println(orderNumber+"."+Settings.space()+
+                        order.getDish().getTitle()+Settings.space()+
+                        order.getDishAmount()+"x"+Settings.space()+"("+
+                        (order.getDish().getPrice().multiply(BigDecimal.valueOf(order.getDishAmount())))+
+                        " Kč"+")"+":"+Settings.tab()+order.getOrderedTime()+
+                        getFulfilmentTimeNotNull(order.getFulfilmentTime())+Settings.tab()+
+                        order.isPaidAsText());
+            }
+            orderNumber++;
+        }
+    }
+    public static BigDecimal getTotalPriceForTable(int numberOfTable, List<Order> listOfOrders){
+        BigDecimal totalprice = BigDecimal.valueOf(0);
+        for (Order order : listOfOrders){
+            if (order.getTableNumber() == numberOfTable){
+                BigDecimal multiply = order.getDish().getPrice().multiply(BigDecimal.valueOf(order.getDishAmount()));
+                totalprice = totalprice.add(multiply);
+            }
+        }
+        return totalprice;
+    }
+
+    private static void getFormattedTableNumber(int tableNumber) {
+        if (tableNumber <= 9){
+            String tableNumberWithSpace = Settings.space() + tableNumber;
+            System.out.println("Objednávky pro stůl č. "+tableNumberWithSpace+":");
+        } else {
+            System.out.println("Objednávky pro stůl č. "+ tableNumber+":");
+        }
+    }
+    private static String getFulfilmentTimeNotNull(LocalTime time){
+        if (time != null){
+            return "-"+String.valueOf(time);
+        }
+        return "";
+    }
+    //endregion
+}
