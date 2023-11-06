303d55a40db1c49bb8eea925a1cc461aa47a50f7
lenkajobankova <lenka.jobi@gmail.com>
2023-11-06 18:31:04 +0100

    Úprava třídy Cookbook
    Ve třídě Cookbook proběhla úprava několika metod, další metody byly doplněny dle zadání projektu.

--- src/com/engeto/restaurant/Cookbook.java
+++ src/com/engeto/restaurant/Cookbook.java
@@ -1,24 +1,19 @@
 package com.engeto.restaurant;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Cookbook {
     List<Dish> cookbook = new ArrayList<>();
 
     //region Constructor
-
-    public Cookbook(List<Dish> cookbook) {
-        this.cookbook.addAll(cookbook);
-    }
+    public Cookbook(){};
 
     //endregion
 
-    //region Getters and Setters
+    //region Methods
     public void addDish(Dish dish){
         cookbook.add(dish);
     }
@@ -36,17 +31,49 @@ public class Cookbook {
         this.cookbook.addAll(cookbook);
     }
 
-    //endregion
-
     public static void saveToFile(String filename, List<Dish> cookbook) throws RestaurantException {
-        try(PrintWriter writer = new PrintWriter(new BufferedWriter(new FileWriter(Settings.fileNameCookbook())))) {
+        try(PrintWriter writer = new PrintWriter(new BufferedWriter(new FileWriter(filename)))) {
             for (Dish dish : cookbook){
-                writer.println(dish.getTitle()+Settings.fileNameCookbook()+dish.getPrice()+Settings.delimiter()+
-                        dish.getPreparationTime()+Settings.delimiter()+dish.getImage());
+                writer.println(dish.getId()+Settings.delimiter()+
+                        dish.getTitle()+Settings.delimiter()+
+                        dish.getPrice()+Settings.delimiter()+
+                        dish.getPreparationTime()+Settings.delimiter()+
+                        dish.getImage());
             }
         } catch (IOException e) {
             throw new RestaurantException("Nastala chyba při zápisu do souboru "+
-                    Settings.fileNameCookbook()+e.getLocalizedMessage());
+                    filename+e.getLocalizedMessage());
         }
     }
+    public static void loadFromFile(String filename) throws RestaurantException {
+        Cookbook result = new Cookbook();
+        int lineNumber = 1;
+        loadFromFileScanner(filename, lineNumber, result);
+    }
+
+    private static void loadFromFileScanner(String filename, int lineNumber, Cookbook result) throws RestaurantException {
+        try(Scanner scanner = new Scanner(new BufferedReader(new FileReader(filename)))){
+            while(scanner.hasNextLine()){
+                String line = scanner.nextLine();
+                String[] blocks = line.split(Settings.delimiter());
+                int numOfBlocks = blocks.length;
+                if(numOfBlocks != 5){
+                    throw new RestaurantException("Špatně zadaný počet položek na řádku č. "+ lineNumber +
+                            " ("+line+"), zadáno: "+numOfBlocks+" položek.");
+                }
+                Dish newDish = new Dish(blocks);
+                result.addDish(newDish);
+                lineNumber++;
+            }
+        } catch (FileNotFoundException e) {
+            throw new RestaurantException("Nastala chyba při čtení ze souboru "+
+                    filename +e.getLocalizedMessage());
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Seznam jídel: " + cookbook;
+    }
+    //endregion
 }
