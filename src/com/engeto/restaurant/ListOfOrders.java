be032881d4091def484c6df6ca3149d70f1f9c57
lenkajobankova <lenka.jobi@gmail.com>
2023-11-06 18:31:35 +0100

    Úprava třídy ListOfOrders
    Ve třídě ListOfOrders proběhla úprava několika metod, další metody byly doplněny dle zadání projektu.

--- src/com/engeto/restaurant/ListOfOrders.java
+++ src/com/engeto/restaurant/ListOfOrders.java
@@ -1,16 +1,17 @@
 package com.engeto.restaurant;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class ListOfOrders {
-    private List<Order> listOfOrders = new ArrayList<>();
-
-    public ListOfOrders(List<Order> listOfOrders) {
+    private final List<Order> listOfOrders = new ArrayList<>();
+    //region Constructor
+    public ListOfOrders(){};
+    //endregion
+    //region Methods
+    public void setListOfOrders(List<Order> listOfOrders) {
         this.listOfOrders.addAll(listOfOrders);
     }
     public void addOrder(Order order){
@@ -26,7 +27,7 @@ public class ListOfOrders {
         return new ArrayList<>(listOfOrders);
     }
     public static void saveToFile(String filename, List<Order> listOfOrders) throws RestaurantException {
-        try(PrintWriter writer = new PrintWriter(new BufferedWriter(new FileWriter(Settings.fileNameOrders())))){
+        try(PrintWriter writer = new PrintWriter(new BufferedWriter(new FileWriter(filename)))){
             for (Order order : listOfOrders){
                 writer.println(order.getTableNumber()+Settings.delimiter()+
                         order.getDish().getId()+Settings.delimiter()+
@@ -36,7 +37,34 @@ public class ListOfOrders {
             }
         } catch (IOException e) {
             throw new RestaurantException("Nastala chyba při zápisu do souboru "+
-                    Settings.fileNameOrders()+e.getLocalizedMessage());
+                    filename+e.getLocalizedMessage());
         }
     }
+    public static void loadFromFile(String filename) throws RestaurantException {
+        ListOfOrders result = new ListOfOrders();
+        int lineNumber = 1;
+        loadFromFileScanner(filename, lineNumber, result);
+    }
+
+    private static void loadFromFileScanner(String filename, int lineNumber, ListOfOrders result) throws RestaurantException {
+        try(Scanner scanner = new Scanner(new BufferedReader(new FileReader(filename)))){
+            while(scanner.hasNextLine()){
+                String line = scanner.nextLine();
+                String[] blocks = line.split(Settings.delimiter());
+                int numOfBlocks = blocks.length;
+                if(numOfBlocks != 5){
+                    throw new RestaurantException("Špatně zadaný počet položek na řádku č. "+ lineNumber +
+                            " ("+line+"), zadáno: "+numOfBlocks+" položek.");
+                }
+                Order newOrder = new Order(blocks);
+                result.addOrder(newOrder);
+                lineNumber++;
+            }
+        } catch (FileNotFoundException e) {
+            throw new RestaurantException("Nastala chyba při čtení ze souboru "+
+                    filename +e.getLocalizedMessage());
+        }
+    }
+    //endregion
+
 }
